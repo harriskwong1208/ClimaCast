f6b4e439db6aff56d94b2be2d6c3d4b58fd28f17
Harris Kwong <harriskwong120898@gmail.com>
2023-08-12 16:07:40 -0400

    Added component inside app component to handle errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react"; 
-
+import weather_api from './API_KEY';
 function App() {
 
   const [Location,setLocation] = useState('tokyo');
-  const [data,setData] = useState('tokyo');
+  const [Data,setData] = useState('tokyo');
+
+  /*           WHEN TRYING TO RUN            */
+  //Replace weather_api and enter your own api key after logging to https://home.openweathermap.org/api_keys
+  const api_key = weather_api;
 
     function getData(e){
       e.preventDefault();
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=b846899c971836d60ba5246751690a5a`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=${api_key}`)
         .then(res => res.json())
         .then(json => setData(json));
   
@@ -34,15 +38,30 @@ function App() {
         </form>
       </div>
       <div>
-        {data.cod == 200 ? 
-        (<pre>{ JSON.stringify(data,null,2)}</pre>) :
-        (<div>Invalid input</div>)}
+        <Listdata data={Data}/>
       </div>
     </nav>
     
   );
 }
+function Listdata(props){
+  const data = props.data.cod;
+  //if blank
+  if(data == 400){
+    return ('');
+  }
+  else if(data == 404){
+    return (
+      'Invalid location'
+    );
+  }
+  else{
+    return (
+      <pre>{ JSON.stringify(props.data,null,2)}</pre>
 
+    );
+  }
+}
 
 
 
